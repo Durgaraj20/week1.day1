e8a8083172b27b547e852c0c09e2d12193fb7039
Durgaraj20 <91733042+Durgaraj20@users.noreply.github.com>
2021-10-03 10:26:12 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int input=5;
+		int fact=1;
+		int i=1;
+		/*
+		 * for (int i = 1; i <=input; i++) { fact=fact*i;
+		 * 
+		 * }
+		 */
+		while (i<=input) {
+			fact=fact*i;
+			i++;
+		}
+	System.out.println(fact);	
+
+	}
+
+}
